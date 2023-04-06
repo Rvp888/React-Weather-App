5bdff19e4ed5322ba7f88f71f03f2328193e836c
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-06 19:44:10 +0530

    modify files

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -3,10 +3,12 @@ import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons';
 
 function Inputs() {
   return (
-    <div>
-      
+    <div className='flex flex-row justify-center my-6'>
+        <div className='flex flex-row w-3/4 items-center justify-center space-x-4'>
+
+        </div>
     </div>
-  )
+  );
 }
 
 export default Inputs;
