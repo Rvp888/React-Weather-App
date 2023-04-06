ede6452182ac614be44b9b80d71754dc8bc6e34b
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-06 21:37:17 +0530

    modify files

--- src/components/TemperatureAndDetails.jsx
+++ src/components/TemperatureAndDetails.jsx
@@ -17,7 +17,19 @@ function TemperatureAndDetails() {
         <p className="text-5xl">34°</p>
         <div className="flex flex-col space-y-2">
           <div className='flex font-light text-sm items-center justify-center'>
-
+            <UilTemperature size={18} className='mr-1' />
+            Real fell:
+            <span className='font-medium ml-1'>32°</span>
+          </div>
+          <div className='flex font-light text-sm items-center justify-center'>
+            <UilTear size={18} className='mr-1' />
+            Humidity:
+            <span className='font-medium ml-1'>65%</span>
+          </div>
+          <div className='flex font-light text-sm items-center justify-center'>
+            <UilWind size={18} className='mr-1' />
+            Wind:
+            <span className='font-medium ml-1'>11 km/h</span>
           </div>
         </div>
       </div>
