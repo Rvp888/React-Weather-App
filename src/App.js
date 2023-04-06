396f1bc42ad59366050a25cef77765a874b973cd
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-06 19:36:06 +0530

    modify files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import UilReact from '@iconscout/react-unicons/icons/uil-react';
 import TopButtons from './components/TopButtons';
+import Inputs from './components/Inputs';
 
 
 
@@ -10,6 +11,7 @@ function App() {
     <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl
     shadow-gray-400'>
       <TopButtons />
+      <Inputs />
     </div>
   );
 }
---
+++ src/components/Inputs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons';
+
+function Inputs() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Inputs;
