1e40b8991eb761e145cff4d29525945122fb8e68
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-06 19:53:31 +0530

    modify files

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -3,10 +3,14 @@ import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons';
 
 function Inputs() {
   return (
-    <div className='flex flex-row justify-center my-6'>
-        <div className='flex flex-row w-3/4 items-center justify-center space-x-4'>
-
-        </div>
+    <div className="flex flex-row justify-center my-6">
+      <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
+        <input
+          type="text"
+          placeholder="Search...."
+          className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize"
+        />
+      </div>
     </div>
   );
 }
