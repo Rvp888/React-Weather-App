ce476c02112d45efa2f47fbfae6e69fdd3695dfd
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-06 19:26:26 +0530

    modify files

--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -26,11 +26,13 @@ function TopButtons() {
   ]
 
 
-  return (
-    <div>
-      
-    </div>
-  )
+  return <div className='flex items-center justify-around my-6'>
+    {cities.map((city) => (
+
+      <button className='text-white text-lg font-medium'>{city.title}</button>
+
+    ))}
+  </div>
 }
 
 export default TopButtons;
