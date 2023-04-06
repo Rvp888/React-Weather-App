df9f4583d3f043f07717c3c8e46a7098e341c801
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-07 00:26:58 +0530

    modify files

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -3,5 +3,10 @@ const API_KEY = '77732bafc06ab64bccebc0f14288c1fe';
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 const getWeatherData = (infoType, searchParams) => {
-    const url = new URL(BASE_URL + infoType)
+    const url = new URL(BASE_URL + "/" + infoType);
+    url.search = new URLSearchParams({...searchParams, appid: API_KEY});
+
+    return fetch(url).then((res) => res.json()).then((data) => data);
 }
+
+export default getWeatherData;
