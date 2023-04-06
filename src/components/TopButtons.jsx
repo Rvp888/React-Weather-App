66c879a383664acfb176f0b237f910783b8aa279
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-06 19:19:49 +0530

    first commit

---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function TopButtons() {
+
+  const cities = [
+    {
+      id: 1,
+      title: 'London'
+    },
+    {
+      id: 2,
+      title: 'Sydney'
+    },
+    {
+      id: 3,
+      title: 'Tokio'
+    },
+    {
+      id: 4,
+      title: 'Toronto'
+    },
+    {
+      id: 5,
+      title: 'Paris'
+    },
+  ]
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TopButtons;
