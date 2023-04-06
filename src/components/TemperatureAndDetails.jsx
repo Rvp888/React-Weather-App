9c49023d7aafa2ee1c3ac0e502c780f48d667e27
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-06 21:14:52 +0530

    modify files

--- src/components/TemperatureAndDetails.jsx
+++ src/components/TemperatureAndDetails.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 function TemperatureAndDetails() {
   return (
     <div>
-      
+        <div className='flex items-center justify-center py-6 text-xl text-cyan-300'>
+            <p>Cloudy or whatever</p>
+        </div>
+        <div className='flex flex-row items-center justify-between text-white py-3'>
+
+        </div>
     </div>
   )
 }
