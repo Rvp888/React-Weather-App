bb97a8c8db9cbb67d9fdff2687b8b63769d8b942
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-07 14:46:10 +0530

    modify files

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ import TimeAndLocation from './components/TimeAndLocation';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
 import Forecast from './components/Forecast';
 import getFormattedWeatherData from './services/weatherService';
+import { useState } from 'react';
 
 
 
 
 function App() {
+
+  const [query, setQuery] = useState({q: "berlin"});
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
 
   const fetchWeather = async() => {
     const data = await getFormattedWeatherData( { q: "london" } );
