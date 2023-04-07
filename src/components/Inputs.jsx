86fe6d20a441e0c0d6f1205b316dc5b3615d5fa9
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-07 17:08:05 +0530

    modify files

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -6,6 +6,11 @@ function Inputs({setQuery, units, setUnits}) {
 
   const [city, setCity] = useState("");
 
+  const handleUnitsChange = (e) => {
+    const selectedUnit = e.currentTarget.name;
+    if(units !== selectedUnit) setUnits(selectedUnit);
+  }
+
   const handleSearchClick = () => {
     if (city !== "") setQuery({q: city})
   }
@@ -45,11 +50,11 @@ function Inputs({setQuery, units, setUnits}) {
       </div>
 
       <div className="flex flex-row w-1/4 items-center justify-center">
-        <button name="metric" className="text-xl text-white font-light transition ease-out hover:scale-125">
+        <button name="metric" className="text-xl text-white font-light transition ease-out hover:scale-125" onClick={handleUnitsChange}>
           °C
         </button>
         <p className='text-xl text-white mx-1'>|</p>
-        <button name="imperial" className="text-xl text-white font-light transition ease-out hover:scale-125">
+        <button name="imperial" className="text-xl text-white font-light transition ease-out hover:scale-125" onClick={handleUnitsChange}>
           °F
         </button>
       </div>
