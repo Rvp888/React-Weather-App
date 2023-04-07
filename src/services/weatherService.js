c573cb9c5ea9fa6ea8bef938ff4c9569fc807a24
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-07 11:55:04 +0530

    modify files

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -31,9 +31,21 @@ const formatForecastWeather = (data) => {
     let { timezone, daily, hourly } = data;
     daily = daily.slice(1,6).map(d => {
         return {
-            title: formatToLocalTime(d.dt, timezone, 'ccc')
+            title: formatToLocalTime(d.dt, timezone, 'ccc'),
+            temp: d.temp.day,
+            icon: d.weather[0].icon,
         }
-    })
+    });
+
+    hourly = hourly.slice(1,6).map(d => {
+        return {
+            title: formatToLocalTime(d.dt, timezone, 'hh:mm a'),
+            temp: d.temp.day,
+            icon: d.weather[0].icon,
+        }
+    });
+
+    return { timezone, daily, hourly };
 }
 
 
