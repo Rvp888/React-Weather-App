805119f024e57dce5cd083d605cee9e3a54ec461
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-07 15:23:07 +0530

    modify files

--- src/components/TemperatureAndDetails.jsx
+++ src/components/TemperatureAndDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { UilArrowUp, UilArrowDown, UilTemperature, UilTear, UilWind, UilSun, UilSunset } from "@iconscout/react-unicons";
+import { formatToLocalTime, iconUrlFromCode } from '../services/weatherService';
 
 
 function TemperatureAndDetails({weather: {
@@ -12,26 +13,26 @@ function TemperatureAndDetails({weather: {
       </div>
       <div className="flex flex-row items-center justify-between text-white py-3">
         <img
-          src="http://openweathermap.org/img/wn/01d@2x.png"
+          src={iconUrlFromCode(icon)}
           alt=""
           className="w-20"
         />
-        <p className="text-5xl">34°</p>
+        <p className="text-5xl">{`${temp.toFixed()}°`}</p>
         <div className="flex flex-col space-y-2">
           <div className='flex font-light text-sm items-center justify-center'>
             <UilTemperature size={18} className='mr-1' />
             Real fell:
-            <span className='font-medium ml-1'>32°</span>
+            <span className='font-medium ml-1'>{`${feels_like.toFixed()}°`}</span>
           </div>
           <div className='flex font-light text-sm items-center justify-center'>
             <UilTear size={18} className='mr-1' />
             Humidity:
-            <span className='font-medium ml-1'>65%</span>
+            <span className='font-medium ml-1'>{`${humidity.toFixed()}%`}</span>
           </div>
           <div className='flex font-light text-sm items-center justify-center'>
             <UilWind size={18} className='mr-1' />
             Wind:
-            <span className='font-medium ml-1'>11 km/h</span>
+            <span className='font-medium ml-1'>{`${speed.toFixed()}km/h`}</span>
           </div>
         </div>
       </div>
@@ -39,7 +40,7 @@ function TemperatureAndDetails({weather: {
       <div className='flex flex-row items-center justify-center space-x-2 text-white text-sm py-3'>
         <UilSun />
         <p className='font-light'>
-            Rise: <span className='font-medium ml-1'>06:45 AM</span>
+            Rise: <span className='font-medium ml-1'>{formatToLocalTime(sunrise, timezone, "hh:mm a")}</span>
         </p>
         <p className='font-light'>|</p>
         <UilSunset />
