c56c5ca70a104463717a727ff5a295a40b68679f
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-07 10:58:24 +0530

    modify files

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -20,6 +20,8 @@ const formatCurrentWeather = (data) => {
         wind: { speed }
     } = data;
 
+    const { main: details, icon } = weather[0];
+
     return { lat, lon, temp, feels_like, temp_min, temp_max, humidity, name, dt, country, sunrise, sunset, weather, speed }
 }
 
