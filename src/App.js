470d668a2f13b7ddc539af7acf00e3e1cb71f0bf
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-07 16:04:26 +0530

    modify files

--- src/App.js
+++ src/App.js
@@ -35,8 +35,8 @@ function App() {
       className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl
     shadow-gray-400"
     >
-      <TopButtons setQuery />
-      <Inputs setQuery units setUnits />
+      <TopButtons setQuery={setQuery} />
+      <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
 
       {weather && (
         <div>
--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons';
+import { useState } from 'react';
 
-function Inputs() {
+function Inputs({setQuery, units, setUnits}) {
+
+  const [city, setCity] = useState("");
+
+  const handleSearchClick = () => {
+    if (city !== "") setQuery({q: city})
+  }
+
+  const handleLocationClick = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        let lat = position.coords.latitude
+        let lon = position.coords.longitude
+
+        setQuery({ lat, lon });
+      });
+    }
+  }
+
+
   return (
     <div className="flex flex-row justify-center my-6">
       <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
         <input
+          value={city}
+          onChange={(e) => setCity(e.currentTarget.value)}
           type="text"
           placeholder="Search for city...."
           className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase"
@@ -13,10 +35,12 @@ function Inputs() {
         <UilSearch
           size={25}
           className="text-white cursor-pointer transition ease-out hover:scale-125"
+          onClick={handleSearchClick}
         />
         <UilLocationPoint
           size={25}
           className="text-white cursor-pointer transition ease-out hover:scale-125"
+          onClick={handleLocationClick}
         />
       </div>
 
