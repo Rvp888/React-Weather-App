8ff2aab72653add738c846c988efc4fcd4eb62fb
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-07 14:42:24 +0530

    modify files

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -5,7 +5,7 @@ const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 const getWeatherData = (infoType, searchParams) => {
     const url = new URL(BASE_URL + "/" + infoType);
-    url.search = new URLSearchParams({...searchParams, appid: API_KEY});
+    url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
 
     return fetch(url).then((res) => res.json());
 }
@@ -23,26 +23,26 @@ const formatCurrentWeather = (data) => {
 
     const { main: details, icon } = weather[0];
 
-    return { lat, lon, temp, feels_like, temp_min, temp_max, humidity, name, dt, country, sunrise, sunset, details, icon, speed }
+    return { lat, lon, temp, feels_like, temp_min, temp_max, humidity, name, dt, country, sunrise, sunset, details, icon, speed };
 }
 
 
 const formatForecastWeather = (data) => {
     let { timezone, daily, hourly } = data;
-    daily = daily.slice(1,6).map(d => {
+    daily = daily.slice(1,6).map((d) => {
         return {
-            title: formatToLocalTime(d.dt, timezone, 'ccc'),
-            temp: d.temp.day,
+            title: formatToLocalTime(d.dt, timezone, "ccc"),
+            temp: d.temp.day,   
             icon: d.weather[0].icon,
-        }
+        };
     });
 
-    hourly = hourly.slice(1,6).map(d => {
+    hourly = hourly.slice(1,6).map((d) => {
         return {
             title: formatToLocalTime(d.dt, timezone, 'hh:mm a'),
-            temp: d.temp.day,
+            temp: d.temp,
             icon: d.weather[0].icon,
-        }
+        };
     });
 
     return { timezone, daily, hourly };
@@ -68,6 +68,9 @@ const getFormattedWeatherData = async(searchParams) => {
 const formatToLocalTime = (secs, zone, format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a") => DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
 
 
+const iconUrlFromCode = (code) => `http://openweathermap.org/img/wn/${code}@2x.png`;
 
 
 export default getFormattedWeatherData;
+
+export { formatToLocalTime, iconUrlFromCode };
